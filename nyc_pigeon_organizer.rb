e8d66fe437f99d5e741546b6ab170b2b1905fda3
blithe-hacker-0535 <sadiebenn1@gmail.com>
2019-02-19 00:43:31 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons_list = {}
+  data.each do |info, adj_hash|
+    adj_hash.each do |adj, name_arr|
+      name_arr.each do |name|
+        pigeons_list[name] ||= {}
+        pigeons_list[name][info] ||= []
+        pigeons_list[name][info] << adj.to_s
+      end
+    end
+  end
+  return pigeons_list
 end
